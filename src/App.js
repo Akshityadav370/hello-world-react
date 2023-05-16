8370d55fc91f4228613833a594cdee1ee125ee1c
Akshit Yadav <akshit07032001@gmail.com>
2023-05-16 19:18:49 +0530

    first react app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import './Hello.css'
+import './Timings.css'
 
 function App() {
   return (
@@ -7,7 +9,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+          Hello World
         </p>
         <a
           className="App-link"
@@ -15,11 +18,51 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Click here to learn React
         </a>
       </header>
     </div>
   );
+}
+
+export function MyProject() {
+  return (
+    <div id='content'>
+      <h1>First React Project</h1>
+      <p>This is my first react component</p>
+      <ul>
+        <li>Red Tape</li>
+        <li>Nike</li>
+        <li>Puma</li>
+      </ul>
+    </div>
+  );
+}
+
+export function Timings() {
+  var time = new Date();
+  var hours = time.getHours();
+  var mins = time.getMinutes();
+  var secs = time.getSeconds();
+  if (hours<=11 && mins<=59 && secs<=59){
+    return (
+      <div id = "breakfast">
+        <h1>Breakfast Time!</h1>
+      </div>
+    );
+  }else if(hours<=17 && mins<=59 && secs<=59){
+    return (
+    <span id="lunch">
+      <span>Lunch Time!</span>
+      <br></br>
+      <span>Lets eat fish curry!</span>
+    </span>);
+  }else{
+    return (<div id="dinner">
+      <h1>Dinner Time!</h1>
+      <span>Lets eat fish!</span>
+    </div>);
+  }
 }
 
 export default App;
